14ff9f3958d8be100aa446b852521c20a68369e7
Young Jun Park <june0313@gmail.com>
2016-05-15 11:37:53 +0900

    add PathVariable to HelloController

--- src/main/java/com/june/hello/HelloController.java
+++ src/main/java/com/june/hello/HelloController.java
@@ -1,7 +1,9 @@
 package com.june.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HelloController {
-
 	@ResponseBody
-	@RequestMapping("/hello")
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
 		return "hello controller!";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+	public String helloWithName(@PathVariable String name) {
+		return "hello " + name;
 	}
 }

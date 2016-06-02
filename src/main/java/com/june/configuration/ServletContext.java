0c42eadbe31915efaf741a0ed3cf94f6bfee708a
Young Jun Park <june0313@gmail.com>
2016-06-02 22:22:19 +0900

    bootstarp cdn 추가, resource handler 추가, HomeController 추가

--- src/main/java/com/june/configuration/ServletContext.java
+++ src/main/java/com/june/configuration/ServletContext.java
@@ -5,18 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by wayne on 2016. 5. 21..
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.june.simplesns.web")
-public class ServletContext {
+public class ServletContext extends WebMvcConfigurerAdapter {
 	@Bean
 	public ViewResolver viewResolver() {
 		HandlebarsViewResolver handlebarsViewResolver = new HandlebarsViewResolver();
 		handlebarsViewResolver.setPrefix("/WEB-INF/view");
 		handlebarsViewResolver.setSuffix(".hbs");
 		return handlebarsViewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/css/**").addResourceLocations("/WEB-INF/css/");
 	}
 }
--- src/main/java/com/june/simplesns/web/home/HomeController.java
+++ src/main/java/com/june/simplesns/web/home/HomeController.java
@@ -1,7 +1,19 @@
 package com.june.simplesns.web.home;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by wayne on 2016. 5. 26..
+ *
  */
+
+@Controller
+@RequestMapping(value = "/")
 public class HomeController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
 }
